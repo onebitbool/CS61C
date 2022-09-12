dea3678483c0dd3a0eb54303a1fdfce0755866ed
onebitbool <onebitbool@gmail.com>
2022-09-12 15:15:06 +0800

    Finish exercise4 in lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,16 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fast = head;
+    node *slow = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow) {
+            return 1;
+        }
+    }
+    
+    return 0;
 }
