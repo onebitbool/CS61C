c451df6ea671a41bfa00e0dc83c5047483fdbfc4
onebitbool <onebitbool@gmail.com>
2022-09-12 11:53:51 +0800

    Finish exercise1 in lab01

---
+++ lab01/README.md
@@ -0,0 +1,7 @@
+# lab01
+## Progess
+1. - [x] exercise1
+2. - [ ] exercise2
+3. - [ ] exercise3
+4. - [ ] exercise4
+5. - [ ] exercise5
--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -14,7 +14,8 @@ int main(int argc, char **argv) {
     int num_z = num_occurrences(str, 'z');
     assert(num_z == 0);
 
-    /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    int num_space = num_occurrences(str, ' ');
+    assert(num_space == 1);
 
     printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
 
@@ -35,7 +36,13 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.G_count == 0);
     assert(dna_seq_2.T_count == 0);
 
-    /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_2.sequence, "CAAGTATCG");
+    compute_nucleotide_occurrences(&dna_seq_2);
+    assert(dna_seq_2.A_count == 3);
+    assert(dna_seq_2.C_count == 2);
+    assert(dna_seq_2.G_count == 2);
+    assert(dna_seq_2.T_count == 2);
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
